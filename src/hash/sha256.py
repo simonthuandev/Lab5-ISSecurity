ab735fb4f12e14aad857f002bfb87b6facfddda7
simonthuandev-cachy <nguyenducthuank588@gmail.com>
2026-05-03 17:22:27 +0700

    docs(sha256_hash): make the docstring shorter

--- src/hash/sha256.py
+++ src/hash/sha256.py
@@ -2,15 +2,7 @@ import hashlib
 
 
 def sha256_hash(text):
-    """
-    Calculate the SHA-256 hash of the given text.
-
-    Args:
-        text (str): The input text to hash.
-
-    Returns:
-        str: The SHA-256 hash of the input text.
-    """
+    """Tính SHA-256 hash của chuỗi văn bản."""
     if not text.strip():
         raise ValueError("input cannot be empty!")
     return hashlib.sha256(text.encode("utf-8")).hexdigest()
